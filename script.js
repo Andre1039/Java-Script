87223a3e30ab9cef991bdf9bf147552898711764
andrey <andreyten2006@gmail.com>
2021-08-12 20:10:43 +0500

    java-script

---
+++ script.js
@@ -0,0 +1,42 @@
+var name = prompt('Введите своё имя');
+
+var age = +prompt('Введите свой возраст');
+
+var text1 = 'Решите пример 2 + 3 = ?' ;
+var text2 = 'Решите пример 8 - 4 = ?' ;
+var text3 = 'Решите пример 6 * 6 = ?' ;
+var text4 = 'Решите пример 24 / 6 = ?' ;
+var text5 = 'Решите пример 23 % 5 = ?' ;
+
+var task1 = +prompt(text1);
+
+console.log('Сложение 2 + 3' , 'Ваш ответ ' , task1);
+
+console.log('Правильный ответ ' , (2 + 3));
+
+var task2 = +prompt(text2);
+
+console.log('Вычитание 8 - 4' , 'Ваш ответ ' , task2);
+
+console.log('Правильный ответ ' , (8 - 4));
+
+var task3 = +prompt(text3);
+
+console.log('Умножение 6 * 6' , 'Ваш ответ ' , task3);
+
+console.log('Правильный ответ ' , 6 * 6);
+
+var task4 = +prompt(text4);
+
+console.log('Деление  24 / 6' , 'Ваш ответ ' , task4);
+
+console.log('Правильный ответ ' , 24 / 6);
+
+var task5 = +prompt(text5);
+
+console.log('Деление с остатком 23 % 5' , 'Ваш ответ ' , task5);
+
+console.log('Правильный ответ ' , 23 % 5);
+
+alert('Перейдите в консоль рфзработчика');
+
